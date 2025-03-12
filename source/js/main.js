d29c5e15263c9d32eebe6945e21c3fd2e52f0f80
eka-jashina <142108910+eka-jashina@users.noreply.github.com>
2025-03-12 12:29:00 +0000

    добавляет секцию price

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,18 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+function openPrice(evt, priceNumber) {
+  var i, priceList, priceTab;
+
+  priceList = document.getElementsByClassName("price__list");
+  for (i = 0; i < priceList.length; i++) {
+    priceList[i].style.display = "none";
+  }
+
+  priceTab = document.getElementsByClassName("price__tab");
+  for (i = 0; i < priceTab.length; i++) {
+    priceTab[i].className = priceTab[i].className.replace(" active", "");
+  }
+
+  document.getElementById(priceNumber).style.display = "grid";
+  evt.currentTarget.className += " active";
+} 
+
+document.getElementById("defaultPriceOpen").click();
