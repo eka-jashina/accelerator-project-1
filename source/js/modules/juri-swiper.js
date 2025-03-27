3202f0721da5f55846283c794b017f1cd117041c
eka-jashina <eka.jashina@gmail.com>
2025-03-27 22:38:38 +0300

    продолжает работу над РР

--- source/js/modules/juri-swiper.js
+++ source/js/modules/juri-swiper.js
@@ -1,5 +1,19 @@
 import {Swiper} from '../vendor/swiper.js';
 
+function fixTabIndex() {
+  document.querySelectorAll(".swiper-slide").forEach((slide) => {
+    slide.setAttribute("tabindex", "-1"); // Отключаем фокус для всех
+  });
+
+  const activeSlides = document.querySelectorAll(".swiper-slide-active, .swiper-slide-next, .swiper-slide-prev");
+  activeSlides.forEach((slide) => {
+    slide.setAttribute("tabindex", "0"); // Разрешаем фокус только на видимых
+  });
+}
+
+// Вызываем сразу, чтобы на первом слайде Tab работал нормально
+fixTabIndex();
+
 function initJuriSwiper() {
   const juriSwiper = new Swiper(".juri__swiper", {
     loop: true,
@@ -27,6 +41,12 @@ function initJuriSwiper() {
     keyboard: {
       enabled: true,
       onlyInViewport: true,
+    },
+
+    on: {
+      slideChangeTransitionEnd: function () {
+        fixTabIndex();
+      },
     },
   });
 
