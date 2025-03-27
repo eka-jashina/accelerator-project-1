67e44bb63a3ffeb9480159de9344d6ff5e5d6f19
eka-jashina <eka.jashina@gmail.com>
2025-03-27 16:04:37 +0400

    добавляет футер

--- source/js/modules/reviews-swiper.js
+++ source/js/modules/reviews-swiper.js
@@ -2,9 +2,6 @@ import {Swiper} from '../vendor/swiper.js';
 
 function initReviewsSwiper() {
   const reviewsSwiper = new Swiper(".reviews__swiper", {
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-
     navigation: {
       nextEl: ".reviews__swiper-button-next",
       prevEl: ".reviews__swiper-button-prev",
